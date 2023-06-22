54a4208629a66be674c4cefd9e49f3f4df428a15
Nasra Kassim <ninyuchinasra@gmail.com>
2023-06-22 22:21:54 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(superbowls) {
+    const winningSuperbowl = superbowls.find(superbowl => superbowl.result === "W");
+    return winningSuperbowl ? winningSuperbowl.year : undefined;
+  }
